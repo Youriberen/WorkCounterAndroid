a57ee2374d47919b5f403fa9a53e737860a2f2a8
Youri Berentsen <Youriberen@gmail.com>
2020-05-20 11:47:15 +0200

    Git init

---
+++ app/src/main/java/com/youriberen/workcounter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.youriberen.workcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
